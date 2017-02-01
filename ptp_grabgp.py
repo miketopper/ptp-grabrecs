83a8bc254c960b72642fe1a53d7306a3eae2000e
Mike Topper <mike@librarything.com>
2017-01-31 19:38:50 -0500

    fixing url linking

--- ptp_grabgp.py
+++ ptp_grabgp.py
@@ -33,7 +33,7 @@ for movie in movies['Movies']:
             print "** Grabbing " + possible_torrent('a.torrent-info-link').text()
             dl_link = possible_torrent('a.torrent-info-link')[0].attrib['href']
             print "** Download link: " + dl_link
-            dl_url = url + dl_link
+            dl_url = site + dl_link
             os.system('cd ' + watchdir + ' ; curl -s -b ' + cookie_file + ' -O -J ' + '"'+dl_url+'"')
     except KeyError:
         continue

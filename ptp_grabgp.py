6e9f69be7643b96ea753ede63ad6d9caa9c4e512
Mike Topper <mike@librarything.com>
2017-01-31 19:33:28 -0500

    putting it all together

--- ptp_grabgp.py
+++ ptp_grabgp.py
@@ -23,6 +23,16 @@ for movie in movies['Movies']:
         if(movie['GroupingQualities'][0]['Torrents'][0]['Freeleech']):
             print movie['Title'] + " IS FREELEECH!"
             possible_torrent = pq(movie['GroupingQualities'][0]['Torrents'][0]['Title'])
+            if(possible_torrent('a.torrent-info-link--user-downloaded')):
+                print "already downloaded!"
+            if(possible_torrent('a.torrent-info-link--user-seeding')):
+                print "already seeding!"
+
+            print "** Grabbing " + possible_torrent('a.torrent-info-link').text()
+            dl_link = possible_torrent('span.basic-movie-list__torrent__action a[title="Download"]')[0].attrib['href']
+            print "** Download link: " + dl_link
+            #dl_url = url + dl_link
+            #os.system('cd ' + watchdir + ' ; curl -s -b ' + cookie_file + ' -O -J ' + '"'+dl_url+'"')
     except KeyError:
         continue
 
@@ -31,20 +41,7 @@ for movie in movies['Movies']:
 
 
 
-    # if(possible_torrent):
-    #
-    #     if(not possible_torrent('a.torrent-info-link span.torrent-info__download-modifier--free')):
-    #         continue
-    #     if(possible_torrent('a.torrent-info-link--user-downloaded')):
-    #         continue
-    #     if(possible_torrent('a.torrent-info-link--user-seeding')):
-    #         continue
-    #
-    #     print "** Grabbing " + possible_torrent('a.torrent-info-link').text()
-    #     dl_link = possible_torrent('span.basic-movie-list__torrent__action a[title="Download"]')[0].attrib['href']
-    #     print "** Download link: " + dl_link
-    #     dl_url = url + dl_link
-    #     os.system('cd ' + watchdir + ' ; curl -s -b ' + cookie_file + ' -O -J ' + '"'+dl_url+'"')
+
 
 
 
